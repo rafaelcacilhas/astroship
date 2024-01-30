c76aa19190e38a4e607b92b93849fa679c9d43b4
Rafael Cacilhas <rafael.cacilhas@quorum.us>
2024-01-30 10:19:19 -0300

    try server

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import vercelServerless from '@astrojs/vercel/serverless';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap()],
   output: 'server',
-  adapter: vercelServerless(),
+  adapter: vercel()
 });
